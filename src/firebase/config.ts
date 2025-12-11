97691c803ade40e8863b320247c573280f493ddd
smartetude <dreaminterpretsite@gmail.com>
2025-12-11 20:31:25 +0100

    Sécurisation de la clé API Firebase avec une variable d'environnement

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,12 @@
+
+
 export const firebaseConfig = {
   "projectId": "studio-7211608627-13304",
   "appId": "1:630179665774:web:d78093d3d765242c9a137f",
-  "apiKey": "AIzaSyBUBsAC4gdh34jEccEgnqf7Qh1I2EFw6fA",
+  // La clé API est maintenant lue depuis les variables d'environnement.
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-7211608627-13304.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "630179665774"
+  "storageBucket": "studio-7211608627-13304.appspot.com",
+  "messagingSenderId": "630179665774",
+  "measurementId": "G-J61GZ3R2F6"
 };
