c67ae465f7be30b668227af56bd64ade2b5174d0
smartetude <dreaminterpretsite@gmail.com>
2025-12-11 21:54:01 +0100

    Sécurisation complète de la configuration Firebase via les variables d'environnement

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,11 @@
 
-
 export const firebaseConfig = {
-  "projectId": "studio-7211608627-13304",
-  "appId": "1:630179665774:web:d78093d3d765242c9a137f",
-  // La clé API est maintenant lue depuis les variables d'environnement.
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": "studio-7211608627-13304.firebaseapp.com",
-  "storageBucket": "studio-7211608627-13304.appspot.com",
-  "messagingSenderId": "630179665774",
-  "measurementId": "G-J61GZ3R2F6"
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  "measurementId": process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
