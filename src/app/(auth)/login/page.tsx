819386f90f9d4a95282a6b7bf92ae4e39b762843
dreaminterpretsite-collab <dreaminterpretsite@gmail.com>
2025-12-27 03:40:41 +0100

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,20 +1,30 @@
 import { LoginForm } from '@/components/auth/login-form';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import Link from 'next/link';
 
 export default function LoginPage() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle className="text-2xl font-headline">Connexion</CardTitle>
+        <CardTitle className="text-2xl font-headline">
+          Connexion
+        </CardTitle>
         <CardDescription>
-          Entrez votre email pour vous connecter à votre compte.
+          Choisissez une méthode pour vous connecter à votre compte.
         </CardDescription>
       </CardHeader>
+
       <CardContent>
         <LoginForm />
+
         <div className="mt-4 text-center text-sm">
-          Vous n'avez pas de compte?{" "}
+          Vous n&apos;avez pas de compte ?{" "}
           <Link href="/signup" className="underline hover:text-primary">
             Inscrivez-vous
           </Link>
